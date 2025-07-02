118bcf0f2c21cf6e0c3c866814f6a9f10b5ecdcf
Mahesh-addagatla <maheshaddagatla1234@gmail.com>
2025-07-02 17:34:26 +0530

    Refactor Like model to replace userID with username for user identification

--- models/Like.js
+++ models/Like.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const likeSchema = new mongoose.Schema({
-  userID: {
-    type: mongoose.Schema.Types.ObjectId,
+  username: {
+    type: String,
     required: true,
     ref: 'User',
   },
